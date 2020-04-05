55b51458cc8d80bb0a9319b66061222ab8008bfa
adtrex <you@example.com>
2020-04-05 15:09:11 +0100

    data base

--- processregister.php
+++ processregister.php
@@ -1,28 +1,49 @@
-<?php
+<?php session_start();
  
 //collecting Data
-$first_name = $_POST["first_name"];
-$last_name = $_POST["last_name"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-$gender = $_POST["gender"];
-$designation = $_POST["designation"];
-$department = $_POST["department"];
 
+$errorCount = 0;
+$first_name = $_POST["first_name"] != "" ? $_POST['first_name'] : $errorCount++;
+$last_name = $_POST["last_name"] != "" ? $_POST['last_name'] : $errorCount++;
+$email = $_POST["email"] != "" ? $_POST['email'] : $errorCount++;
+$password = $_POST["password"] != "" ? $_POST['password'] : $errorCount++;
+$gender = $_POST["gender"] != "" ? $_POST['gender'] : $errorCount++;
+$designation = $_POST["designation"] != "" ? $_POST['designation'] : $errorCount++;
+$department = $_POST["department"] != "" ? $_POST['department'] : $errorCount++;
 
-$errorArray = [];
+$_SESSION["first_name"] = $first_name;
+$_SESSION["last_name"] = $last_name;
+$_SESSION["email"] = $email;
+$_SESSION["password"] = $password;
+$_SESSION["gender"] = $gender;
+$_SESSION["designation"] = $designation;
+$_SESSION["department"] = $department;
 
-//verifying Data
+ 
 
-if ($first_name == "") {
-	$errorArray = "first_name cannot be blank";
+if ($errorCount > 0) {
+	//Display proper messages to the user
+	//Give more accurate feedback to the user
+	 $_SESSION["error"] = "You have " . $errorCount . " errors in your form submission";
+	 header("Location: register.php");
+}else{
+	$userObject =[
+		'first_name'=>$first_name,
+		'last_name'=>$last_name,
+		'email'=>$email,
+		'password'=>$password,
+		'gender'=>$gender,
+		'designation'=>$designation,
+		'department'=>$department,
+
+	]
+	//Save files in DB)
+	file_put_contents($first_name . $last_name . ".json","Hello World. Testing!");
 }
 
-if ($last_name == "") {
-	$errorArray = "last_name cannot be blank";
-}
 
-print_r($errorArray);
+
+
 
 
 //saving Data
--- register.php
+++ register.php
@@ -1,51 +1,146 @@
 	
-	<?php include_once("header.php"); ?>
+	<?php session_start(); 
+	print_r($_SESSION);
+	include_once("header.php"); ?>
 
 		<p><strong>Welcome, please register</strong></p>
 		<p>All Fields are required</p>
 
 		<form method="POST" action="processregister.php">
+
+			<p>
+				<?php
+
+					if(isset($_SESSION['error']) && !empty($_SESSION["error"])){
+						echo "<span style='color:red'>" . $_SESSION['error']. "</span>";
+						//session_unset();
+						session_destroy();
+					}
+				?>
+			</p>
 			
 			<p>
 				<label>First Name</label><br />
-				<input type="text" name="first_name" placeholder="First Name" required />
+				<input 
+				<?php
+				
+				if(isset($_SESSION['first_name'])){
+							echo "value=" . $_SESSION['first_name'];
+				}
+
+				?>
+
+
+				type="text" name="first_name" placeholder="First Name"  />
 			</p>
 			<p>
 				<label>Last Name</label><br />
-				<input type="text" name="last_name" placeholder="Last Name" required />
+				<input 
+
+				<?php
+				
+				if(isset($_SESSION['last_name'])){
+							echo "value=" . $_SESSION['last_name'];
+				}
+
+				?>
+
+				type="text" name="last_name" placeholder="Last Name"  />
 			</p>
 			<p>
 				<label>Email</label><br />
-				<input type="text" name="email" placeholder="Email" required />
+				<input 
+
+				<?php
+				
+				if(isset($_SESSION['email'])){
+							echo "value=" . $_SESSION['email'];
+				}
+
+				?>
+
+				type="text" name="email" placeholder="Email"  />
 			</p>
 			<p>
 				<label>Password</label><br />
-				<input type="text" name="password" placeholder="Password" required />
+				<input 
+
+				type="text" name="password" placeholder="Password"  />
 			</p>
 
 			<p>
 				<label>Gender</label><br />
-				<select name="gender" required />
+				<select name="gender"  
+				<?php
+				
+				if(isset($_SESSION['first_name'])){
+							echo "value=" . $_SESSION['first_name'];
+				}
+
+				?>
+				>
 					<option value="">Select One</option>
-					<option>Male</option>
-					<option>Female</option>
+					<option
+				<?php
+				
+					if(isset($_SESSION['gender']) && $_SESSION['gender'] == 'Male'){
+					echo "selected";
+					}
+
+				?>
+					>Male</option>
+					<option
+				<?php
+				
+					if(isset($_SESSION['gender']) && $_SESSION['gender'] == 'Female'){
+					echo "selected";
+					}
+
+				?>
+					>Female</option>
 				</select>
 			</p>
 			<hr />
 
 			<p>
 				<label>Designation</label><br />
-				<select name="designation" required />
+				<select name="designation"  />
 					<option value="">Select One</option>
-					<option>Medical Team (MT)</option>
-					<option>Patients</option>
+					<option
+				<?php
+				
+					if(isset($_SESSION['designation']) && $_SESSION['designation'] == 'Medical Team (MT)'){
+					echo "selected";
+					}
+
+				?>
+					>Medical Team (MT)</option>
+					<option
+				<?php
+				
+					if(isset($_SESSION['designation']) && $_SESSION['designation'] == 'Patients'){
+					echo "selected";
+					}
+
+				?>
+					>Patients</option>
 				</select>
 			</p>
 
 
 			<p>
 				<label>Department</label><br />
-				<input type="" name="department" placeholder="Department" required />
+				<input 
+
+				<?php
+				
+				if(isset($_SESSION['department'])){
+							echo "value=" . $_SESSION['department'];
+				}
+
+				?>
+
+				type="" name="department" placeholder="Department"  />
 			</p>
 
 			<p>
